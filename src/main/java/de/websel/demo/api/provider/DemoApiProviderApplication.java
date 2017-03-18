52730a39c4b94a9adca636fd6a5d58bd0562780b
Klaus Mähringer <kmaehringer@websel.de>
2017-03-18 15:57:26 +0100

    initial project

---
+++ src/main/java/de/websel/demo/api/provider/DemoApiProviderApplication.java
@@ -0,0 +1,12 @@
+package de.websel.demo.api.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiProviderApplication.class, args);
+	}
+}
---
+++ src/test/java/de/websel/demo/api/provider/DemoApiProviderApplicationTests.java
@@ -0,0 +1,16 @@
+package de.websel.demo.api.provider;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoApiProviderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
