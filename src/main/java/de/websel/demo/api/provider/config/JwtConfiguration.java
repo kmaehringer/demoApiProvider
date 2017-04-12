34d38299db7c5df3945ecf1e2fa53ec30e93b825
Klaus Mähringer <kmaehringer@websel.de>
2017-04-12 14:44:57 +0200

    changed from key files to properties

--- src/main/java/de/websel/demo/api/provider/config/JwtConfiguration.java
+++ src/main/java/de/websel/demo/api/provider/config/JwtConfiguration.java
@@ -1,23 +1,22 @@
 package de.websel.demo.api.provider.config;
 
-import java.io.IOException;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-import org.springframework.util.FileCopyUtils;
 
 @Configuration
 public class JwtConfiguration {
 	Logger log = LoggerFactory.getLogger(JwtConfiguration.class);
+
+	@Value("${config.oauth2.publicKey}")
+	private String publicKey;
 
 	@Autowired
 	JwtAccessTokenConverter jwtAccessTokenConverter;
@@ -32,13 +31,6 @@ public class JwtConfiguration {
 	@Bean
 	protected JwtAccessTokenConverter jwtTokenEnhancer() {
 		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-		Resource resource = new ClassPathResource("websel.pem");
-		String publicKey = null;
-		try {
-			publicKey = new String(FileCopyUtils.copyToByteArray(resource.getInputStream()));
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
 		converter.setVerifierKey(publicKey);
 		return converter;
 	}
