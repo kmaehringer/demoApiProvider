5e2ef626a1726cbe016c59ee5a2b6d1448360eb0
Klaus Mähringer <kmaehringer@websel.de>
2017-05-12 16:44:05 +0200

    switch to Keycloak as OIDV Provider

--- src/main/java/de/websel/demo/api/provider/controller/FooController.java
+++ src/main/java/de/websel/demo/api/provider/controller/FooController.java
@@ -2,7 +2,6 @@ package de.websel.demo.api.provider.controller;
 
 import java.util.UUID;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/foo")
 public class FooController {
 
-	@PreAuthorize("hasAuthority('FOO_READ')")
 	@RequestMapping(method = RequestMethod.GET)
 	public String readFoo() {
 		return "read foo " + UUID.randomUUID().toString();
 	}
 
-	@PreAuthorize("hasAuthority('FOO_WRITE')")
 	@RequestMapping(method = RequestMethod.POST)
 	public String writeFoo() {
 		return "write foo " + UUID.randomUUID().toString();
--- src/main/java/de/websel/demo/api/provider/controller/UserController.java
+++ src/main/java/de/websel/demo/api/provider/controller/UserController.java
@@ -2,15 +2,15 @@ package de.websel.demo.api.provider.controller;
 
 import java.security.Principal;
 
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-	@RequestMapping("/")
-	@PreAuthorize("hasAuthority('USER')")
+	@RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public Principal resource(Principal principal) {
 		return principal;
 	}
